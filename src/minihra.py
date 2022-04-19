498cf08e8d5a080b7d1ea6c7170156a7dda0fa80
Matouš Morávek <moravekm21@spstrutnov.cz>
2022-04-19 12:22:54 +0200

    matematika_minihra

---
+++ src/minihra.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+
+pygame.init()
+display = pygame.display.set_mode((400, 300))
+myfont = pygame.font.Sysfont("verdana", 20)
+
+mytext = myfont.render("Matematika začíná:", (200, 50, 50))
+
