4142a0e16ad42fbfc1a28f4f5823648db9649fc5
Šimon Wendler <wendlers21@spstrutnov.cz>
2022-04-20 09:23:54 +0200

    kámen, nůžky, papír - funkční podmínky

--- src/minihra.py
+++ src/minihra.py
@@ -3,7 +3,52 @@ import random
 volba = ["kámen", "nůžky", "papír"]
 
 počítač = random.choice(volba)
-hráč = none
+hráč = None
 
-while hráč not in choices:
-    hráč input("kámen", 
+while hráč not in volba:
+    hráč = input("kámen, nůžky, nebo papír?: ").lower()
+    
+    if hráč == počítač:
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Remíza!")
+        
+if hráč == "kámen":
+    if počítač == "papír":
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Prohráváš!")
+        
+if hráč == "nůžky":
+    if počítač == "papír":
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Vyhráváš!")
+        
+if hráč == "papír":
+    if počítač == "kámen":
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Vyhráváš!")
+        
+if hráč == "papír":
+    if počítač == "nůžky":
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Prohráváš!")
+        
+if hráč == "kámen":
+    if počítač == "nůžky":
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Vyhráváš!")
+        
+if hráč == "nůžky":
+    if počítač == "kámen":
+        print("počítač: ", počítač)
+        print("hráč: ", hráč)
+        print("Prohráváš!")
+        
+        
+
+        
