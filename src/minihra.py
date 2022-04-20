453d08fd09a3c9bc2bf3515c81ad27a2e570759b
Šimon Wendler <wendlers21@spstrutnov.cz>
2022-04-20 09:42:33 +0200

    hotový kámen, nůžky, papír :) (bez GUI)

--- src/minihra.py
+++ src/minihra.py
@@ -1,54 +1,63 @@
 import random
 
-volba = ["kámen", "nůžky", "papír"]
+while True:
+    volba = ["kámen", "nůžky", "papír"]
 
-počítač = random.choice(volba)
-hráč = None
+    počítač = random.choice(volba)
+    hráč = None
 
-while hráč not in volba:
-    hráč = input("kámen, nůžky, nebo papír?: ").lower()
+    while hráč not in volba:
+        hráč = input("kámen, nůžky, nebo papír?: ").lower()
     
-    if hráč == počítač:
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Remíza!")
+        if hráč == počítač:
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Remíza!")
         
-if hráč == "kámen":
-    if počítač == "papír":
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Prohráváš!")
+    if hráč == "kámen":
+        if počítač == "papír":
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Prohráváš!")
         
-if hráč == "nůžky":
-    if počítač == "papír":
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Vyhráváš!")
+    if hráč == "nůžky":
+        if počítač == "papír":
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Vyhráváš!")
         
-if hráč == "papír":
-    if počítač == "kámen":
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Vyhráváš!")
+    if hráč == "papír":
+        if počítač == "kámen":
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Vyhráváš!")
         
-if hráč == "papír":
-    if počítač == "nůžky":
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Prohráváš!")
+    if hráč == "papír":
+        if počítač == "nůžky":
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Prohráváš!")
         
-if hráč == "kámen":
-    if počítač == "nůžky":
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Vyhráváš!")
+    if hráč == "kámen":
+        if počítač == "nůžky":
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Vyhráváš!")
         
-if hráč == "nůžky":
-    if počítač == "kámen":
-        print("počítač: ", počítač)
-        print("hráč: ", hráč)
-        print("Prohráváš!")
+    if hráč == "nůžky":
+        if počítač == "kámen":
+            print("počítač: ", počítač)
+            print("hráč: ", hráč)
+            print("Prohráváš!")
         
+    Hrát_znova = input("Hrát znova? (ano/ne): ").lower()     
+        
+    if Hrát_znova !="ano":
+        break
+    
+print("Sbohem")
+    
+
         
 
         
