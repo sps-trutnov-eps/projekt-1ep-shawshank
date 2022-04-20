8dd3d4b8937f590323cf306a218ec1b1e6429059
Matouš Morávek <moravekm21@spstrutnov.cz>
2022-04-20 09:21:35 +0200

    Vypisování textu

--- src/minihra.py
+++ src/minihra.py
@@ -3,15 +3,16 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
 dialogue_font = pygame.font.SysFont("arial", 60)
-dialogue = dialogue_font.render("Matematika začíná!", True, (255, 50, 100))
+dialogue = dialogue_font.render("                Spočítej příklady!                      ", True, (0, 0, 0))
+dialogue2 = dialogue_font.render("                Příklad 1:                      ", True, (0, 0, 0))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-   
-    screen.fill((255, 255, 255))
+            
+
+    screen.fill((195, 155, 119))
      
     screen.blit(dialogue, (40, 40))
      
