d33ef43b29ff4b4d490b3df8af41150a15d9e1a7
Matouš Morávek <moravekm21@spstrutnov.cz>
2022-04-27 11:35:12 +0200

    nehotové vypisování textu

--- src/minihra.py
+++ src/minihra.py
@@ -1,20 +1,53 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
-dialogue_font = pygame.font.SysFont("arial", 60)
-dialogue = dialogue_font.render("                Spočítej příklady!                      ", True, (0, 0, 0))
-dialogue2 = dialogue_font.render("                Příklad 1:                      ", True, (0, 0, 0))
+pygame.init() 
+screen = pygame.display.set_mode((1000, 1000)) 
+ def submt(var1):
+    if var1.get() == str(resultPLUS()):
+        correct = (text="Správně!", fg="green", font=("Courier", 16))
+        correct.place(relx=0.3, rely=0.2)
+    else:
+        wrong = (text="Špatně!!", fg="red", font=("Courier", 16))
+        wrong.place(relx=0.3, rely=0.2)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            
+ 
+while True: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            pygame.quit() 
+             
+ 
+    screen.fill((195, 155, 119)) 
+      
+    screen.blit(dialogue, (40, 40)) 
+      
 
-    screen.fill((195, 155, 119))
-     
-    screen.blit(dialogue, (40, 40))
-     
-    pygame.display.flip()
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def submt(var1):
+    if var1.get() == str(resultPLUS()):
+        correct = (text="Správně!", fg="green", font=("Courier", 16))
+        correct.place(relx=0.3, rely=0.2)
+    else:
+        wrong = (text="Špatně!!", fg="red", font=("Courier", 16))
+        wrong.place(relx=0.3, rely=0.2)
+
+
+
