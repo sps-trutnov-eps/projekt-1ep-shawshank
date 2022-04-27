8f9ce9257aa389233e95cab2f84a000039abcf29
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2022-04-27 11:12:15 +0200

    Odpovědi v GUI

--- src/shawshank_minihra.py
+++ src/shawshank_minihra.py
@@ -19,8 +19,17 @@ def kontrola_kod(seznam,seznam2,typ):
     for polozka in seznam:
         if polozka[typ] == seznam2[typ]:
             return polozka
+def datum_souradnice(text,x):
+    datum = font.render(text, True, (0,0,0))
+    datum_rect = datum.get_rect()
+    datum_rect.center = (x,y_center)
+    return datum, datum_rect
 odpovedi = cteni_dat_list("odpovedi.csv")
 otazky = cteni_dat_list("otazky.csv")
+x1_center = x1 + (w_odp/2)
+x2_center = x2 + (w_odp/2)
+x3_center = x3 + (w_odp/2)
+y_center = y_odp + (h_odp/2)
 i_otazka = random.randint(0,(len(otazky) - 1))
 otazka = otazky[i_otazka]
 odpoved = kontrola_kod(odpovedi,otazka,"kod")
@@ -39,10 +48,13 @@ while false_odpoved_2['kod'] == odpoved['kod'] or false_odpoved_2 == false_odpov
 
 rozliseni_okna = rozliseni_x, rozliseni_y = (1200,800)
 pozadi_barva = (186, 140, 90)
+lokace_1 = random.randint(0,2)
+lokace_2 = random.randint(0,2)
+lokace_3 = random.randint(0,2)
 
-w_pap = rozliseni_x / 2
-h_pap = rozliseni_y * 0.937
-h_odp = 75
+lokace_1 = random.randint(0,2)
+lokace_2 = random.randint(0,2)
+lokace_3 = random.randint(0,2)
 w_odp = 100
 x_pap = (rozliseni_x - w_pap) / 2
 y_pap = (rozliseni_y - h_pap) / 2
@@ -55,7 +67,9 @@ y_odp = y_pap * 24.16
 pygame.init()
 okno = pygame.display.set_mode(rozliseni_okna)
 
-
+datum_1,datum_1_center = datum_souradnice(data[lokace_1],x1_center)
+datum_2,datum_2_center = datum_souradnice(data[lokace_2],x2_center)
+datum_3,datum_3_center = datum_souradnice(data[lokace_3],x3_center)
 
 while True:
     udalosti = pygame.event.get()
@@ -76,4 +90,7 @@ while True:
     pygame.draw.rect(okno, (0, 0, 0), (x1,y_odp,w_odp,h_odp), (5))
     pygame.draw.rect(okno, (0, 0, 0), (x2,y_odp,w_odp,h_odp), (5))
     pygame.draw.rect(okno, (0, 0, 0), (x3,y_odp,w_odp,h_odp), (5))
+    okno.blit(datum_1, datum_1_center)
+    okno.blit(datum_2, datum_2_center)
+    okno.blit(datum_3, datum_3_center)
     pygame.display.update()
