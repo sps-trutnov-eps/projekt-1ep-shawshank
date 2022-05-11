472d4e6ebe86860ef278e8a00abb6e807b7d86b7
Vojtěch Laňka <lankav21@spstrutnov.cz>
2022-05-11 10:59:16 +0200

    Zěměpisná minihra
    Už to funguje, ale není to graficky zpracované.

--- src/minihra.py
+++ src/minihra.py
@@ -14,6 +14,8 @@ handle.close()
 spravne_odpovedi = vsechny_odpovedi.strip().split("\n")
 otazky= vsechny_otazky.strip().split("\n")
 
+#otázky
+
 print("Vítejte u ustního zkoušení ze zěměpisu")
 print("Tak začneme")
 znamka = 6
@@ -21,77 +23,96 @@ pocet_otazek=5
 
 print("Otázka 1:")
 nahodna_otazka1 = random.choice(otazky)
-poradi_otazky = otazky.index(nahodna_otazka1)
-input(nahodna_otazka1)
-spravna_odpoved1 = (spravne_odpovedi[poradi_otazky])
-
-if nahodna_otazka1 == (spravne_odpovedi[poradi_otazky]):
+print(nahodna_otazka1)
+poradi_otazky1 = otazky.index(nahodna_otazka1)
+spravna_odpoved1 = spravne_odpovedi[poradi_otazky1]
+if spravna_odpoved1 == input() :
     print("Správně")
     znamka = znamka - 1
-else:
-    print("Špatně")
-    print(spravna_odpoved1)
     
+else:
+    print("ŠPATNĚ")
     
 print("Otázka 2:")
 nahodna_otazka2 = random.choice(otazky)
-poradi_otazky = otazky.index(nahodna_otazka2)
-input(nahodna_otazka2)
-spravna_odpoved1 = (spravne_odpovedi[poradi_otazky])
-
-if nahodna_otazka2 == (spravne_odpovedi[poradi_otazky]):
+print(nahodna_otazka2)
+poradi_otazky2 = otazky.index(nahodna_otazka2)
+spravna_odpoved2 = spravne_odpovedi[poradi_otazky2]
+if spravna_odpoved2 == input() :
     print("Správně")
     znamka = znamka - 1
+    
 else:
-    print("Špatně")
-    print(spravna_odpoved1)    
-
+    print("ŠPATNĚ")
+    
 print("Otázka 3:")
 nahodna_otazka3 = random.choice(otazky)
-poradi_otazky = otazky.index(nahodna_otazka3)
-input(nahodna_otazka3)
-spravna_odpoved1 = (spravne_odpovedi[poradi_otazky])
-
-if nahodna_otazka3 == (spravne_odpovedi[poradi_otazky]):
+print(nahodna_otazka3)
+poradi_otazky3 = otazky.index(nahodna_otazka3)
+spravna_odpoved3 = spravne_odpovedi[poradi_otazky3]
+if spravna_odpoved3 == input() :
     print("Správně")
     znamka = znamka - 1
+    
 else:
-    print("Špatně")
-    print(spravna_odpoved1)    
-
+    print("ŠPATNĚ")
+    
 print("Otázka 4:")
 nahodna_otazka4 = random.choice(otazky)
-poradi_otazky = otazky.index(nahodna_otazka4)
-input(nahodna_otazka4)
-spravna_odpoved1 = (spravne_odpovedi[poradi_otazky])
-
-if nahodna_otazka4 == (spravne_odpovedi[poradi_otazky]):
+print(nahodna_otazka4)
+poradi_otazky4 = otazky.index(nahodna_otazka4)
+spravna_odpoved4 = spravne_odpovedi[poradi_otazky4]
+if spravna_odpoved4 == input() :
     print("Správně")
-    znamka = znamka - 1
+    znamka = znamka -1 
+    
 else:
-    print("Špatně")
-    print(spravna_odpoved1)    
-
+    print("ŠPATNĚ")
+    
 print("Otázka 5:")
 nahodna_otazka5 = random.choice(otazky)
-poradi_otazky = otazky.index(nahodna_otazka5)
-input(nahodna_otazka5)
-spravna_odpoved1 = (spravne_odpovedi[poradi_otazky])
-
-if nahodna_otazka5 == (spravne_odpovedi[poradi_otazky]):
+print(nahodna_otazka5)
+poradi_otazky5 = otazky.index(nahodna_otazka5)
+spravna_odpoved5 = spravne_odpovedi[poradi_otazky5]
+if spravna_odpoved5 == input() :
     print("Správně")
     znamka = znamka - 1
+    
 else:
-    print("Špatně")
-    print(spravna_odpoved1)
-
-
-
-
-if znamka == 6:
-    print("Dostal jste za 5 <_>")
+    print("ŠPATNĚ")      
+    
+    
+#konecna-znamka    
+    
+if znamka == 6 :
+    print("Hodně špatný výkon, za 5 !!!")
+    
 else:
-    print("Dostal jste", znamka)
+    print("Dostal si za", znamka)
+    
+if znamka < 5:
+    život = True
+    
+else:
+    život = False
+    
+    
+if život == True:
+    print("Super! Neumřel jsi!!!")
+    
+if život == False:
+    print("Tak tady končíme...")
+    
+    
+    
+    
+    
+
+
+
+
+
+
 
 
 
