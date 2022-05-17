99ad44e1793942c030b09713683c272b5430d3cd
Karel Kříž <KrizK21@spstrutnov.cz>
2022-05-17 13:16:22 +0200

    Na doma

---
+++ src/inventory.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+inventoryImages = pygame.image.load("../data/inventory/inventory.png")
+hasKeys = False
+hasBoots = False
+canGoOut = False
+
+
+def playerAnim():
+            
+    return(image)
