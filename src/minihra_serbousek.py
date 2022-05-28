19376793fb8efeaeca17cd6bf04ec04f4a24c3a7
Jan Serbousek <serbousek.jan@seznam.cz>
2022-05-28 23:02:09 +0200

    Cyklus procházení abecedy a její vykreslení

--- src/minihra_serbousek.py
+++ src/minihra_serbousek.py
@@ -4,44 +4,54 @@ window = pygame.display.set_mode((640,480))
 pygame.display.update()
 pygame.display.set_caption("Šibenice")
 game_over = False
+abeceda = [
+    {"znak": "A", "stav": True},
+    {"znak": "B", "stav": True},
+    {"znak": "C", "stav": False},
+    {"znak": "D", "stav": True},
+    {"znak": "E", "stav": True},
+    {"znak": "F", "stav": True},
+    {"znak": "G", "stav": False},
+    {"znak": "H", "stav": False},
+    {"znak": "I", "stav": False},
+    {"znak": "J", "stav": True},
+    {"znak": "K", "stav": True},
+    {"znak": "L", "stav": False},
+    {"znak": "M", "stav": True},
+    {"znak": "N", "stav": True},
+    {"znak": "O", "stav": True},
+    {"znak": "P", "stav": True},
+    {"znak": "Q", "stav": False},
+    {"znak": "R", "stav": True},
+    {"znak": "S", "stav": True},
+    {"znak": "T", "stav": True},
+    {"znak": "U", "stav": True},
+    {"znak": "V", "stav": True},
+    {"znak": "W", "stav": True},
+    {"znak": "X", "stav": True},
+    {"znak": "Y", "stav": True},
+    {"znak": "Z", "stav": True}
+    ]
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+            quit()
     color = (0,44,0)
     white = (255, 255, 255)
     X = 640
     Y = 480
-    display_surface = pygame.display.set_mode((X, Y))
     window.fill(color)
     font = pygame.font.SysFont('forte', 26)
-    text = font.render("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", True, white)
-    textRect = text.get_rect()
-    textRect.center = (X // 2, Y // 1.05)
-    while True:
-        display_surface.fill
-        display_surface.blit(text, textRect)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-        pygame.display.update()
-        
-        
-                     
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-        
-    
-
+    odsazeni = 10
+    for pismeno in abeceda:
+        if pismeno["stav"] == True:
+            text = font.render(pismeno["znak"],True,white)
+        elif pismeno["stav"] == False:
+            text = font.render("_",True,white)
+        textRect = text.get_rect()
+        rozmery_pismene = font.size(pismeno["znak"])
+        textRect.center = (rozmery_pismene[0]//2 + odsazeni, Y // 1.05)
+        window.blit(text, textRect)
+        odsazeni += rozmery_pismene[0] + 7.5
+    pygame.display.update() 
