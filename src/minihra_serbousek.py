bc019284cb30852c311deba20a7708ee68580b45
Jan Serbousek <serbousek.jan@seznam.cz>
2022-06-04 22:38:40 +0200

    Ovládání označení - je možnost posouvat doprava

--- src/minihra_serbousek.py
+++ src/minihra_serbousek.py
@@ -5,32 +5,32 @@ pygame.display.update()
 pygame.display.set_caption("Šibenice")
 game_over = False
 abeceda = [
-    {"znak": "A", "stav": True},
-    {"znak": "B", "stav": True},
-    {"znak": "C", "stav": False},
-    {"znak": "D", "stav": True},
-    {"znak": "E", "stav": True},
-    {"znak": "F", "stav": True},
-    {"znak": "G", "stav": False},
-    {"znak": "H", "stav": False},
-    {"znak": "I", "stav": False},
-    {"znak": "J", "stav": True},
-    {"znak": "K", "stav": True},
-    {"znak": "L", "stav": False},
-    {"znak": "M", "stav": True},
-    {"znak": "N", "stav": True},
-    {"znak": "O", "stav": True},
-    {"znak": "P", "stav": True},
-    {"znak": "Q", "stav": False},
-    {"znak": "R", "stav": True},
-    {"znak": "S", "stav": True},
-    {"znak": "T", "stav": True},
-    {"znak": "U", "stav": True},
-    {"znak": "V", "stav": True},
-    {"znak": "W", "stav": True},
-    {"znak": "X", "stav": True},
-    {"znak": "Y", "stav": True},
-    {"znak": "Z", "stav": True}
+    {"znak": "A", "stav": True , "stav2": True},
+    {"znak": "B", "stav": True , "stav2": False},
+    {"znak": "C", "stav": True , "stav2": False},
+    {"znak": "D", "stav": False , "stav2": False},
+    {"znak": "E", "stav": True , "stav2": False},
+    {"znak": "F", "stav": True , "stav2": False},
+    {"znak": "G", "stav": True , "stav2": False},
+    {"znak": "H", "stav": True , "stav2": False},
+    {"znak": "I", "stav": True , "stav2": False},
+    {"znak": "J", "stav": True , "stav2": False},
+    {"znak": "K", "stav": True , "stav2": False},
+    {"znak": "L", "stav": True , "stav2": False},
+    {"znak": "M", "stav": True , "stav2": False},
+    {"znak": "N", "stav": True , "stav2": False},
+    {"znak": "O", "stav": True , "stav2": False},
+    {"znak": "P", "stav": True , "stav2": False},
+    {"znak": "Q", "stav": True , "stav2": False},
+    {"znak": "R", "stav": True , "stav2": False},
+    {"znak": "S", "stav": True , "stav2": False},
+    {"znak": "T", "stav": True , "stav2": False},
+    {"znak": "U", "stav": True , "stav2": False},
+    {"znak": "V", "stav": True , "stav2": False},
+    {"znak": "W", "stav": True , "stav2": False},
+    {"znak": "X", "stav": True , "stav2": False},
+    {"znak": "Y", "stav": True , "stav2": False},
+    {"znak": "Z", "stav": True , "stav2": False}
     ]
 while not game_over:
     for event in pygame.event.get():
@@ -39,6 +39,7 @@ while not game_over:
             quit()
     color = (0,44,0)
     white = (255, 255, 255)
+    red = (255, 0, 0)
     X = 640
     Y = 480
     window.fill(color)
@@ -46,7 +47,10 @@ while not game_over:
     odsazeni = 10
     for pismeno in abeceda:
         if pismeno["stav"] == True:
-            text = font.render(pismeno["znak"],True,white)
+            if pismeno["stav2"] == False:
+                text = font.render(pismeno["znak"],True,white)
+            elif pismeno["stav2"] == True:
+                text = font.render(pismeno["znak"],True,red)
         elif pismeno["stav"] == False:
             text = font.render("_",True,white)
         textRect = text.get_rect()
@@ -54,4 +58,17 @@ while not game_over:
         textRect.center = (rozmery_pismene[0]//2 + odsazeni, Y // 1.05)
         window.blit(text, textRect)
         odsazeni += rozmery_pismene[0] + 7.5
-    pygame.display.update() 
+    stisk = pygame.key.get_pressed()
+    if stisk[pygame.K_RIGHT]:
+        for p, pismeno in enumerate(abeceda):
+            if pismeno["stav2"] == False:
+                pass
+            elif pismeno["stav2"] == True:
+                pozice = p
+                break
+        abeceda[pozice]["stav2"] = False
+        if pozice == 25:
+            abeceda[0]["stav2"] = True
+        else:
+            abeceda[pozice+1]["stav2"] = True
+    pygame.display.update()  
