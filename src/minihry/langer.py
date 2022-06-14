57069128480b8d5c2fea579d2c4888b0da2fdd78
Karel Kříž <KrizK21@spstrutnov.cz>
2022-06-14 13:22:11 +0200

    opraveno

--- src/minihry/langer.py
+++ src/minihry/langer.py
@@ -88,8 +88,9 @@ def vyberPrikladu():
 def main():
     global hotovozInt, rozliseni, font1, font2, font3, font4, nadpis, vysledek, prikladuSpatne, prikladuDobre, hotovoz, screen, timeOut, vyberPozice, vyberVelikost, p, priklad, prikladText, spravnyVyber, spravnaOdpoved, spatnaOdpoved1, spatnaOdpoved2, spravnaOdpovedText, spatnaOdpovedText1, spatnaOdpovedText2
     vyberPrikladu()
-
-
+    
+    pygame.display.set_mode(rozliseni)
+    
     while True:
         stisknuto = pygame.key.get_pressed()
         for event in pygame.event.get():
@@ -100,43 +101,43 @@ def main():
                 pygame.quit()
                 sys.exit()
             
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if pygame.mouse.get_pos()[0] > vyberPozice[0] and pygame.mouse.get_pos()[0] < vyberPozice[0] + vyberVelikost[0] and pygame.mouse.get_pos()[1] > vyberPozice[1] and pygame.mouse.get_pos()[1] < vyberPozice[1] + vyberVelikost[1]:
-                if timeOut == 0 and hotovozInt < 10:
-                    if spravnyVyber == 0:
-                        hotovozInt += 1
-                        prikladuDobre += 1
-                    else:
-                        hotovozInt += 1
-                        prikladuSpatne += 1
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if pygame.mouse.get_pos()[0] > vyberPozice[0] and pygame.mouse.get_pos()[0] < vyberPozice[0] + vyberVelikost[0] and pygame.mouse.get_pos()[1] > vyberPozice[1] and pygame.mouse.get_pos()[1] < vyberPozice[1] + vyberVelikost[1]:
+                    if timeOut == 0 and hotovozInt < 10:
+                        if spravnyVyber == 0:
+                            hotovozInt += 1
+                            prikladuDobre += 1
+                        else:
+                            hotovozInt += 1
+                            prikladuSpatne += 1
+                            
+                        hotovoz = font4.render(f"{hotovozInt}/10", True, (0, 0, 0))
+                        timeOut = 500
+                        vyberPrikladu()
+                if pygame.mouse.get_pos()[0] > vyberPozice[0] + vyberVelikost[0]+20 and pygame.mouse.get_pos()[0] < vyberPozice[0] + vyberVelikost[0]+20 + vyberVelikost[0] and pygame.mouse.get_pos()[1] > vyberPozice[1] and pygame.mouse.get_pos()[1] < vyberPozice[1] + vyberVelikost[1]:
+                    if timeOut == 0 and hotovozInt < 10:
+                        if spravnyVyber == 1:
+                            hotovozInt += 1
+                            prikladuDobre += 1
+                        else:
+                            hotovozInt += 1
+                            prikladuSpatne += 1
                         
-                    hotovoz = font4.render(f"{hotovozInt}/10", True, (0, 0, 0))
-                    timeOut = 500
-                    vyberPrikladu()
-            if pygame.mouse.get_pos()[0] > vyberPozice[0] + vyberVelikost[0]+20 and pygame.mouse.get_pos()[0] < vyberPozice[0] + vyberVelikost[0]+20 + vyberVelikost[0] and pygame.mouse.get_pos()[1] > vyberPozice[1] and pygame.mouse.get_pos()[1] < vyberPozice[1] + vyberVelikost[1]:
-                if timeOut == 0 and hotovozInt < 10:
-                    if spravnyVyber == 1:
-                        hotovozInt += 1
-                        prikladuDobre += 1
-                    else:
-                        hotovozInt += 1
-                        prikladuSpatne += 1
-                    
-                    hotovoz = font4.render(f"{hotovozInt}/10", True, (0, 0, 0))
-                    timeOut = 500
-                    vyberPrikladu()
-            if pygame.mouse.get_pos()[0] > vyberPozice[0] + vyberVelikost[0]*2+40 and pygame.mouse.get_pos()[0] < vyberPozice[0] + vyberVelikost[0]*2+40 + vyberVelikost[0] and pygame.mouse.get_pos()[1] > vyberPozice[1] and pygame.mouse.get_pos()[1] < vyberPozice[1] + vyberVelikost[1]:
-                if timeOut == 0 and hotovozInt < 10:
-                    if spravnyVyber == 2:
-                        hotovozInt += 1
-                        prikladuDobre += 1
-                    else:
-                        hotovozInt += 1
-                        prikladuSpatne += 1
-                    
-                    hotovoz = font4.render(f"{hotovozInt}/10", True, (0, 0, 0))
-                    timeOut = 300
-                    vyberPrikladu()
+                        hotovoz = font4.render(f"{hotovozInt}/10", True, (0, 0, 0))
+                        timeOut = 500
+                        vyberPrikladu()
+                if pygame.mouse.get_pos()[0] > vyberPozice[0] + vyberVelikost[0]*2+40 and pygame.mouse.get_pos()[0] < vyberPozice[0] + vyberVelikost[0]*2+40 + vyberVelikost[0] and pygame.mouse.get_pos()[1] > vyberPozice[1] and pygame.mouse.get_pos()[1] < vyberPozice[1] + vyberVelikost[1]:
+                    if timeOut == 0 and hotovozInt < 10:
+                        if spravnyVyber == 2:
+                            hotovozInt += 1
+                            prikladuDobre += 1
+                        else:
+                            hotovozInt += 1
+                            prikladuSpatne += 1
+                        
+                        hotovoz = font4.render(f"{hotovozInt}/10", True, (0, 0, 0))
+                        timeOut = 300
+                        vyberPrikladu()
             
         screen.fill((4, 75, 240))
         pygame.draw.rect(screen, (220, 220, 220), (8, 8, 170, 30))
