2b781608a70530a3823e9a83782a4eb61a75ef80
Vojtěch Nepimach <nepimachv21@spstrutnov.cz>
2022-06-17 11:14:36 +0200

    Implementován kód na cheaty
    Kód na zapnutí / vypnutí cheatů je "dev" + menší tweak rychlosti titulků.

--- src/main.py
+++ src/main.py
@@ -72,6 +72,7 @@ health_max = health = 5
 mozne_prechody = []
 interactive = pygame.sprite.Group()
 player_movable = True
+cheat_code = None
 
 default_time = 60
 current_time = 60
@@ -449,7 +450,6 @@ while True:
                 elif menu_state != 2: menu_state += 1
                 else: menu_state = 0
             cheat_timeout = 10
-        cheat_timeout -=1
         
         janitor_menu_grp.update()
         janitor_menu_grp.draw(screen)
@@ -539,13 +539,19 @@ while True:
         elif playerStartGameAnim and player_hitbox_instance.rect.centerx >= 23*32/2:
             playerStartGameAnim = False
             player_movable = True
-            
+         
+        cheat_timeout -= 1
+         
         #cheaty
-        if pressed[pygame.K_SEMICOLON] and cheat_timeout < 0:
+        if pressed[pygame.K_d]:
+            cheat_code = 1
+        if pressed[pygame.K_e] and cheat_code == 1:
+            cheat_code = 2
+        if pressed[pygame.K_v] and cheat_code == 2:
             if cheaty: cheaty = False
             else: cheaty = True
             cheat_timeout = 20
-        cheat_timeout -= 1
+            cheat_code = None
 
         if cheaty:
             if pressed[pygame.K_h] and cheat_timeout < 0:
@@ -605,33 +611,34 @@ while True:
                     player_speed = 3
                 cheat_timeout = 20
         
-        if pressed[pygame.K_t] and cheat_timeout < 0:
-            current_time = default_time
-            hall.stop()
-            hall.play()
-            cheat_timeout = 20
-            
-        if pressed[pygame.K_1] and cheat_timeout < 0:
-            inventoryKey_grp.update()
-            cheat_timeout = 20
-            
-        if pressed[pygame.K_2] and cheat_timeout < 0:
-            inventoryBoots_grp.update("sebrat")
-            cheat_timeout = 20
-            
-        if pressed[pygame.K_3] and cheat_timeout < 0:
-            inGame = False
-            win = True
-            cheat_timeout = 20
-            hall.stop()
-            
-        if pressed[pygame.K_p] and cheat_timeout < 0:
-            if show_minigame == False:
-                show_minigame = True
-            else:
-                show_minigame = False
-            cheat_timeout = 20
-        cheat_timeout -= 1
+            elif pressed[pygame.K_t] and cheat_timeout < 0:
+                current_time = default_time
+                hall.stop()
+                hall.play()
+                cheat_timeout = 20
+                
+            elif pressed[pygame.K_1] and cheat_timeout < 0:
+                inventoryKey_grp.update()
+                cheat_timeout = 20
+                
+            elif pressed[pygame.K_2] and cheat_timeout < 0:
+                inventoryBoots_grp.update("sebrat")
+                cheat_timeout = 20
+                
+            elif pressed[pygame.K_3] and cheat_timeout < 0:
+                inGame = False
+                win = True
+                cheat_timeout = 20
+                hall.stop()
+                
+            elif pressed[pygame.K_p] and cheat_timeout < 0:
+                if show_minigame == False:
+                    show_minigame = True
+                else:
+                    show_minigame = False
+                cheat_timeout = 20
+        
+        print(cheat_code)
         
         #pohyb
         posun_x = 0
@@ -969,7 +976,7 @@ Ardour
 
 '''
         screen.fill("black")
-        delta_y -= 1
+        delta_y -= 2
         centerx, centery = screen.get_rect().centerx, screen.get_rect().centery
         if pressed[pygame.K_RETURN]:
             Credits = False
