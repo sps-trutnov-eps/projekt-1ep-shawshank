86ca504f54d367139efe90e832f4a386c30a977f
Vojtěch Nepimach <nepimachv21@spstrutnov.cz>
2022-06-19 13:15:34 +0200

    Ikona pro build i build_onefile + oprava životů v mainu
    Ikona se generuje pro build i pro build_onefile, ale jen, když je shawshank.exe v \bin\dist\shawshank.exe. Ikona se také generuje pokaždé a ukazuje se hned po generaci exáče.

--- src/main.py
+++ src/main.py
@@ -74,7 +74,7 @@ cheaty = False
 player_x = 23 * 32 / 2
 player_y = 14 * 32 / 2
 player_speed = 3
-health_max = health = 1
+health_max = health = 5
 mozne_prechody = []
 interactive = pygame.sprite.Group()
 player_movable = True
